748306e66bc02205e517b712ba214e9c7f5c86fa
AZelinsky96 <azelinsky1996@gmail.com>
2019-05-28 17:59:26 -0400

    update1

--- dictionary.py
+++ dictionary.py
@@ -17,27 +17,49 @@ import os
 
 
 def loading_json():
+    """
+    This is the function that will load the json into play.
+    """
     return json.load(open("data.json", "r"))
 
 
 
 def adding_word(word):
+    """
+    This function will be used to add the word to the data file with the input definition.
+    """
+    ## Loading the json in using function above
     data = loading_json()
+    ## Creating a data insert list
     data_insert = list()
+    ## Appending the values of the user input for word definition
     data_insert.append(str(input("Input definition of word {}: ".format(word))))
+    ## Creating a new data entry for the input data above
     data['{}'.format(word)] = data_insert
+    ## Appending the data to the data file
     with open("data.json", "w") as f:
         json.dump(data, f)
 
 def finding_word():
+    """
+    This is the main function of the program. This will be used to input a word, and look it up within the data file.
+
+    """
+    ## Accepting user input
     word = input("Please enter the word you wish to learn more about: ").lower()
+    ## Loading in the data file
     data = loading_json()
+
     try:
+        ## This will print the word and associated definition
         data[word]
-        return print("Word: {}\nDefinition: {}".format(word, data[word][0]))
+        return print("Word: {}\nDefinition: {}\n".format(word, data[word][0]))
     except:
+        ## If the word is not located within the data file. It will inform user that it is not located within data file and ask to insert the word.
         print("That word does not exist in the dictionary.")
+        ## Asking the user if they would like to add the word.
         add_it = input("Would you like to add the word? [y/n]: ").lower()
+        ## Flow control for add it key
         if (add_it != "y") and (add_it != 'n'):
             while True:
                 add_it = input("Error! Please Enter y or n")
@@ -55,6 +77,7 @@ def dictionary():
     while True:
         finding_word()
 
+        ## Once the finding word function executes, it will ask to include another word. 
         break_word = input("Would you like to lookup another word? [y/n]").lower()
 
         if (break_word != "y") and (break_word != "n"):
